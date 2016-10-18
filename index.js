d2a1703864cba1ee4457d4e834db41f404658c55
Giuseppe Menti <mentifg@gmail.com>
2016-10-18 12:26:44 -0200

    feat: Remove condition to check if status is 200

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/harvests', async (req, res) => {
   try {
     ({ data, status } = await request(source, render))
   } catch (e) {
-    console.log('1', e.response)
+    console.log('1', e)
     return res.status(e.response.status).json({
       error: e.response.data
     })
